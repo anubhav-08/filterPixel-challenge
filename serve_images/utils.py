d45748d5278b07966b88f49bef3c750e5ef5c3bf
Anubhav Tyagi <tyagianubhav80@gmail.com>
2023-01-16 00:25:28 +0530

    google images url api created

--- serve_images/utils.py
+++ serve_images/utils.py
@@ -3,7 +3,6 @@ import botocore
 from botocore import UNSIGNED
 from botocore.config import Config
 import os
-# from Google import Create_Service
 
 
 def fetch_s3_images():
@@ -14,7 +13,7 @@ def fetch_s3_images():
     s3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))
     my_bucket = s3.Bucket('testbucketfp')
     for my_bucket_object in my_bucket.objects.all():
-        print(my_bucket_object.key)
+        # print(my_bucket_object.key)
         response = s3_client.generate_presigned_url('get_object',
                                                         Params={'Bucket': 'testbucketfp',
                                                                 'Key': my_bucket_object.key},
@@ -25,41 +24,60 @@ def fetch_s3_images():
 
 # Completing in a while  
 
-# def fetch_google_images():
 
-#     CLIENT_SECRET_FILE = 'client-secret.json'
-#     API_NAME = 'drive'
-#     API_VERSION = 'v3'
-#     SCOPES = ['https://www.googleapis.com/auth/drive']
+# from __future__ import print_function
 
-#     service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+import os.path
 
-#     # Update Sharing Setting
-#     file_id = '<file id>'
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-#     request_body = {
-#         'role': 'reader',
-#         'type': 'anyone'
-#     }
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
-#     response_permission = service.permissions().create(
-#         fileId=file_id,
-#         body=request_body
-#     ).execute()
+def fetch_google_images():
 
-#     print(response_permission)
-    
-#     # Print Sharing URL
-#     response_share_link = service.files().get(
-#         fileId=file_id,
-#         fields='webViewLink'
-#     ).execute()
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'serve_images\credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
-#     print(response_share_link)
+    try:
+        service = build('drive', 'v3', credentials=creds)
 
-#     # Remove Sharing Permission
-#     service.permissions().delete(
-#         fileId=file_id,
-#         permissionId='anyoneWithLink'
-#     ).execute()
+        # Call the Drive v3 API
+        folder_id = '1_qOJ0z3kI_e2IJq4X6HqF0T1ROBESygS' # Same folder ID as above
+        query = "'%s' in parents" % folder_id
+        results = service.files().list(q=query, pageSize=1000, 
+                                        includeItemsFromAllDrives=True, 
+                                        supportsAllDrives=True, 
+                                        corpora="allDrives", 
+                                        fields='files(webViewLink)').execute()
+        items = results.get('files', [])
 
+        if not items:
+            print('No files found.')
+            return
+        response = []
+        for item in items:
+            response.append(item['webViewLink'])
+        return response
+    except HttpError as error:
+        # TODO(developer) - Handle errors from drive API.
+        print(f'An error occurred: {error}')
--- serve_images/views.py
+++ serve_images/views.py
@@ -1,16 +1,14 @@
 from rest_framework.response import Response
 from rest_framework import generics
 from .serializers import S3ImagesSerializer
-from .utils import fetch_s3_images
+from .utils import fetch_s3_images, fetch_google_images
 
 class S3ImagesView(generics.GenericAPIView):
     def get(self, request):
-        data = fetch_s3_images()
-        print(data)
-        return Response({'data' : data})
+        img_urls = fetch_s3_images()
+        return Response({'data' : img_urls})
 
 class GoogleImagesView(generics.GenericAPIView):
     def get(self, request):
-        data = fetch_s3_images()
-        print(data)
-        return Response({'data' : data})
+        img_urls = fetch_google_images()
+        return Response({'data' : img_urls})
