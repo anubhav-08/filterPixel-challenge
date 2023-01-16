32554869bcfd8a64403e7711348bd79eac6557fd
Anubhav Tyagi <tyagianubhav80@gmail.com>
2023-01-16 11:45:57 +0530

    updated images api views

--- filterPixel/settings.py
+++ filterPixel/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
 
     #Third party
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL=True
 
 ROOT_URLCONF = 'filterPixel.urls'
 
--- serve_images/views.py
+++ serve_images/views.py
@@ -2,13 +2,15 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .serializers import S3ImagesSerializer
 from .utils import fetch_s3_images, fetch_google_images
+import json
 
 class S3ImagesView(generics.GenericAPIView):
     def get(self, request):
-        img_urls = fetch_s3_images()
-        return Response({'data' : img_urls})
+        img_urls = json.dumps(fetch_s3_images())
+
+        return Response(img_urls)
 
 class GoogleImagesView(generics.GenericAPIView):
     def get(self, request):
-        img_urls = fetch_google_images()
-        return Response({'data' : img_urls})
+        img_urls = json.dumps(fetch_google_images())
+        return Response(img_urls)
